d7b67a3ea6a0b5b3e2c153565a72c7cd68efe922
David Baeza <david.isaac.baeza@gmail.com>
2021-02-12 20:34:36 -0600

    React Redux Form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ConfirgureStore } from './redux/configureStore';
+import { ConfigureStore } from './redux/configureStore';
 import './App.css';
 
-const store = ConfirgureStore();
+const store = ConfigureStore();
 
 class App extends Component {
     render() {
